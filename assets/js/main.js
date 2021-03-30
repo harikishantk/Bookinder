90748d479a3bf6ccf22ad6e97ce224df35b1dd5d
R Sowjanya <60657099+sowjanya-105@users.noreply.github.com>
2021-03-30 11:41:22 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Gp - v2.2.1
-* Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
